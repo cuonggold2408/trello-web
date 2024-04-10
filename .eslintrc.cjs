6129046802246466aaf0d4c4e9783e22df728f2f
cuonggold2408 <cuonggold2408@gmail.com>
2024-04-11 00:11:03 +0700

    update project structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react", "react-hooks", "react-refresh"],
   rules: {
+    // React
     "react-refresh/only-export-components": [
       "warn",
       { allowNamedExports: true },
@@ -21,6 +22,15 @@ module.exports = {
     "react/prop-types": 0,
     "react/display-name": 0,
 
+    // MUI
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["@mui/*/*/*"],
+      },
+    ],
+
+    // Common
     "no-console": 1,
     "no-lonely-if": 1,
     "no-unused-vars": 1,
