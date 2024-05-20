84a7c07ccb4ce40e351fbabcc1b6cc7aa90d235f
cuonggold2408 <cuonggold2408@gmail.com>
2024-05-20 11:39:20 +0700

    test deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = "http://localhost:8017/api";
+let apiRoot = "";
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = process.env.API_LOCAL;
+}
+
+if (process.env.BUILD_MODE === "production") {
+  apiRoot = process.env.API_PRODUCTION;
+}
+
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-
+import dotenv from "dotenv";
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+    API_LOCAL: `"${process.env.API_LOCAL}"`,
+    API_PRODUCTION: `"${process.env.API_PRODUCTION}"`,
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "~", replacement: "/src" }],
